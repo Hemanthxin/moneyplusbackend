08f2cd1e521cd974553c39efe39d0278927e94f5
Hemanthxin <hemanthb293@gmail.com>
2026-07-20 14:47:50 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-from app.main import app
+from pathlib import Path
+import sys
 
+CURRENT_DIR = Path(__file__).resolve().parent
+if str(CURRENT_DIR) not in sys.path:
+    sys.path.insert(0, str(CURRENT_DIR))
+
+from app.main import app
